3b3d06f6c166184caaf6c6cb77406353b5004ee3
Anasyaseen92 <anasy7148@gmail.com>
2025-04-22 10:13:46 -0700

    header desiging done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import Profile from './pages/Profile';
+import About from './pages/About';
+import SignUp from './pages/SignUp';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <>
-    <div className='text-red-500'>App</div>
-    <p>shfshdkshdisad</p>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
     </>
-  )
+  );
 }
