93e32dd13489b2fcc524226b36b300e9d599ecb7
Anasyaseen92 <anasy7148@gmail.com>
2025-04-22 10:47:52 -0700

    abc

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
-import {Link} from 'react-router-dom'
-
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-        <Link to='/'>
-        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-          <span className="text-slate-500">Sahand</span>
-          <span className="text-slate-700">Estate</span>
-        </h1>
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">Sahand</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
         </Link>
         <form className="bg-slate-100 p-3 rounded-lg flex icons-start items-center">
           <input
@@ -22,20 +21,20 @@ export default function Header() {
           <FaSearch className="text-slate-600" />
         </form>
         <ul className="flex gap-4">
-          <Link to='/'>
-          <li className="hidden sm:inline text-slate-700  hover:underline">
-            Home
-          </li>
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700  hover:underline">
+              Home
+            </li>
           </Link>
-          <Link to='/about'>
-          <li className="hidden sm:inline text-slate-700  hover:underline">
-            About
-          </li>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700  hover:underline">
+              About
+            </li>
           </Link>
-          <Link to='/sign-in'>
-          <li className="hidden sm:inline text-slate-700  hover:underline">
-            Sign In
-          </li>
+          <Link to="/sign-in">
+            <li className="hidden sm:inline text-slate-700  hover:underline">
+              Sign In
+            </li>
           </Link>
         </ul>
       </div>
