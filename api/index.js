2b6b36d8857f6c17d75c114e45150a7003f2baac
Anasyaseen92 <anasy7148@gmail.com>
2025-04-23 06:35:42 -0700

    create user model

--- api/index.js
+++ api/index.js
@@ -1,23 +1,23 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO)
-  .then(() => {
-    console.log("Chlda pya a");
-  })
-  .catch((err) => {
-    console.error("MongoDB connection failed:", err.message);
-  });
-
-
 const app = express();
 
+mongoose.connect("mongodb+srv://anas:anas123456@mern-estate-cluster0.qvkxhyt.mongodb.net/mern-estate?retryWrites=true&w=majority&appName=mern-estate-cluster0")
+  .then(() => {
+    console.log("Chlda pya a");
+    // Add another log after the connection is successful
+    console.log("MongoDB connection successful!");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection failed:", err);
+    console.error("Error Details:", err.stack);
+  });
+
 app.listen(3000, () => {
-  console.log("server is running");
-  console.log(process.env.MONGO);
-
-
+  console.log("Server is running on port 3000");
+  console.log("MongoDB URL: ", process.env.MONGO);  // Logging the MongoDB connection string
 });
