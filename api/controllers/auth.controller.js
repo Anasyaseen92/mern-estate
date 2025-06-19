9d0f55cfbd8542274540a596b28371e738225f6c
Anasyaseen92 <anasy7148@gmail.com>
2025-06-19 13:36:48 -0700

    complete header search form functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,18 +1,18 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { errorHandler } from '../utils/error.js';
+import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
-  console.log('Creating user:', newUser);
+  console.log("Creating user:", newUser);
   try {
     await newUser.save();
     res.status(201).json("successfullay Done");
   } catch (error) {
-    console.error('Signup error:', error);
+    console.error("Signup error:", error);
     next(error);
   }
 };
@@ -26,14 +26,14 @@ export const signin = async (req, res, next) => {
     if (!validPassword) return next(errorHandler(401, "wrong credential"));
 
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
-      expiresIn: '1d',
+      expiresIn: "1d",
     });
 
     const { password: pass, ...rest } = validUser._doc;
     res
       .cookie("access_token", token, { httpOnly: true })
       .status(200)
-      .json({...rest, _id: validUser._id});
+      .json({ ...rest, _id: validUser._id });
   } catch (error) {
     next(error);
   }
@@ -44,19 +44,22 @@ export const google = async (req, res, next) => {
     const user = await User.findOne({ email: req.body.email });
     if (user) {
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-        expiresIn: '1d',
+        expiresIn: "1d",
       });
       const { password: pass, ...rest } = user._doc;
       res
         .cookie("access_token", token, { httpOnly: true })
         .status(200)
-        .json({...rest,_id :user._id});
+        .json({ ...rest, _id: user._id });
     } else {
       const generatedPassword =
-        Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
       const hashPassword = bcryptjs.hashSync(generatedPassword, 10);
       const newUser = new User({
-        username: req.body.name.split(" ").join("").toLowerCase() + Math.random().toString(36).slice(-4),
+        username:
+          req.body.name.split(" ").join("").toLowerCase() +
+          Math.random().toString(36).slice(-4),
         email: req.body.email,
         password: hashPassword,
         avatar: req.body.photo,
@@ -64,25 +67,26 @@ export const google = async (req, res, next) => {
       await newUser.save();
 
       const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-        expiresIn: '1d',
+        expiresIn: "1d",
       });
 
       const { password: pass, ...rest } = newUser._doc;
       res
         .cookie("access_token", token, { httpOnly: true })
         .status(200)
-        .json({ ...rest, _id: newUser._id });3
+        .json({ ...rest, _id: newUser._id });
+      3;
     }
   } catch (error) {
     next(error);
   }
 };
 
-export const signOut= async (req, res, next)=>{
-try {
-  res.clearCookie('access_token');
-  res.status(200).json('User has been sign out!');
-} catch (error) {
-  next(error)
-}
-}
+export const signOut = async (req, res, next) => {
+  try {
+    res.clearCookie("access_token");
+    res.status(200).json("User has been sign out!");
+  } catch (error) {
+    next(error);
+  }
+};
